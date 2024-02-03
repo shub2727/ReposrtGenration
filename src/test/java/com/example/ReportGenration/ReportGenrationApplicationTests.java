68aa413cdf01ef4ae7d33c32e162dca466105f7b
Shubham gude <shubhamgude90@gmail.com>
2024-02-03 14:15:19 +0530

    testfile

--- src/test/java/com/example/ReportGenration/ReportGenrationApplicationTests.java
+++ src/test/java/com/example/ReportGenration/ReportGenrationApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.ReportGenration;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest (classes =ReportGenrationApplication.class )
 class ReportGenrationApplicationTests {
 
 	@Test
