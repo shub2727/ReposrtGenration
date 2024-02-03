5304747afdd1d37229e9157e96954168d6274050
Shubham gude <shubhamgude90@gmail.com>
2024-02-03 14:33:06 +0530

    commentout

--- src/test/java/com/example/ReportGenration/ReportGenrationApplicationTests.java
+++ src/test/java/com/example/ReportGenration/ReportGenrationApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.ReportGenration;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest (classes =ReportGenrationApplication.class )
-class ReportGenrationApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.example.ReportGenration;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest (classes =ReportGenrationApplication.class )
+//class ReportGenrationApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
